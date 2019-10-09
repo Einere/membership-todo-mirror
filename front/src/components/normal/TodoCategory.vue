674f5be9a9e023f1d0af747598d3f2414f953495
Einenre <kjwsx23@gmail.com>
2019-10-09 22:26:53 +0900

    feat(category): implement modify category name feature
    카테고리 이름을 변경할 수 있게 하기 위해, 카테고리 이름 변경 기능을 구현함.
    
    Signed-off-by: Einenre <kjwsx23@gmail.com>

--- front/src/components/normal/TodoCategory.vue
+++ front/src/components/normal/TodoCategory.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="category-sub-container">
-    <h3>{{category.name}}</h3>
+    <section class="category-content">
+      <h3 v-if="!categoryTitleEdit">{{category.name}}</h3>
+      <input type="text" v-model="category.name" v-else>
+      <font-awesome-icon icon="edit" @click="editCategoryTitle" v-if="!categoryTitleEdit"/>
+      <font-awesome-icon icon="save" @click="saveCategoryTitle" v-else/>
+      <font-awesome-icon icon="trash-alt"/>
+    </section>
     <!-- offset 계산을 정상적으로 하기 위해, self 조건을 추가 -->
     <section class="note-container" @dragover.prevent.self="dragoverHandler"
              @drop.prevent.self="dropHandler">
@@ -21,6 +27,7 @@
         data() {
             return {
                 notes: [],
+                categoryTitleEdit: false,
             };
         },
         computed: {
@@ -29,20 +36,36 @@
             }
         },
         methods: {
+            editCategoryTitle() {
+                this.categoryTitleEdit = true;
+            },
+            saveCategoryTitle() {
+                this.categoryTitleEdit = false;
+
+                fetch(`${this.$store.state.baseURL}/todo/category/name/${this.category.id}/${this.category.name}`, {
+                    method: 'PATCH',
+                    credentials: "include",
+                })
+                    .then(result => result.json())
+                    .then(result => console.log(result))
+                    .catch(error => console.log(error));
+            },
             dragoverHandler(ev) {
             },
             dropHandler(ev) {
                 // 대상의 id를 가져와 대상 DOM에 움직인 요소를 추가합니다.
-                const id = ev.dataTransfer.getData("text/plain");
+                const moveData = JSON.parse(ev.dataTransfer.getData("text/plain"));
+                moveData.to = this.category.id;
+                moveData.newPosition = this.getProperPosition(this.getProperIndex(ev.offsetY));
 
                 // category component에서 해당 note를 제거한 후, 새 category에 넣는다.
-                this.$emit('deleteNote', parseInt(id, 10), this.category.id, this.getProperPosition(this.getProperIndex(ev.offsetY)));
+                this.$emit('deleteNote', moveData);
             },
-            deleteNote(noteId, to, newPosition) {
-                const index = this.notes.findIndex(note => note.id === noteId);
+            deleteNote(moveData) {
+                const index = this.notes.findIndex(note => note.id === moveData.noteId);
                 if (index > -1) {
                     const [note] = this.notes.splice(index, 1);
-                    this.$emit('addNote', note, to, newPosition);
+                    this.$emit('addNote', note, moveData);
                 }
             },
             addNote(note, newPosition) {
@@ -83,11 +106,25 @@
 </script>
 
 <style scoped>
+
+
+  .category-content {
+    height: 5rem;
+    border: 1px black solid;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    min-height: fit-content;
+    overflow: hidden;
+    flex-grow: 0;
+  }
+
   .category-sub-container {
     display: flex;
     flex-direction: column;
     align-items: center;
     width: unset;
+    max-width: 25%;
     height: -webkit-fill-available;
     margin: 0.5rem;
   }
